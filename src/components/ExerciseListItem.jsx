85c1c4ce8a615ca78573500dcd394e74f1926ae9
Eden Pearson <pearsoneden@gmail.com>
2024-03-06 10:53:06 +1300

    add all exercises

---
+++ src/components/ExerciseListItem.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ExerciseListItem({ item }) {
+  return (
+    <View style={styles.exerciseContainer}>
+      <Text style={styles.exerciseName}>{item.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        <Text style={styles.subValue}>{item.muscle}</Text> |{' '}
+        <Text style={styles.subValue}>{item.equipment}</Text>
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  exerciseContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  exerciseSubtitle: {
+    color: 'gray',
+  },
+  subValue: {
+    textTransform: 'capitalize',
+  },
+})
