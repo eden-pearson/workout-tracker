465999c77a1fb7cefea7832d19eb4fb093a0f307
Eden Pearson <pearsoneden@gmail.com>
2024-03-06 12:39:08 +1300

    add details screen

---
+++ src/app/[name].jsx
@@ -0,0 +1,45 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { useLocalSearchParams } from 'expo-router'
+import exercises from '../../assets/data/exercises.json'
+import { Stack } from 'expo-router'
+
+export default function ExerciseDetailsScreen() {
+  const params = useLocalSearchParams()
+
+  const exercise = exercises.find((item) => item.name === params.name)
+
+  if (!exercise) {
+    return <Text>Exercise not found </Text>
+  }
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Stack.Screen options={{ title: exercise.name }} />
+      <Text style={styles.exerciseName}>{exercise.name}</Text>
+      <Text style={styles.instructions}>
+        Instructions: {exercise.instructions}
+      </Text>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    gap: 10,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  exerciseSubtitle: {
+    color: 'gray',
+  },
+  subValue: {
+    textTransform: 'capitalize',
+  },
+  instructions: {
+    fontSize: 16,
+    lineHeight: 20,
+  },
+})
--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { Link } from 'expo-router'
 
 export default function ExerciseListItem({ item }) {
   return (
-    <View style={styles.exerciseContainer}>
-      <Text style={styles.exerciseName}>{item.name}</Text>
-      <Text style={styles.exerciseSubtitle}>
-        <Text style={styles.subValue}>{item.muscle}</Text> |{' '}
-        <Text style={styles.subValue}>{item.equipment}</Text>
-      </Text>
-    </View>
+    <Link href={`/${item.name}`} asChild>
+      <Pressable style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{item.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          <Text style={styles.subValue}>{item.muscle}</Text> |
+          <Text style={styles.subValue}>{item.equipment}</Text>
+        </Text>
+      </Pressable>
+    </Link>
   )
 }
 
@@ -18,6 +21,17 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     gap: 5,
+    marginHorizontal: 2,
+    // shadow
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
   },
   exerciseName: {
     fontSize: 20,
