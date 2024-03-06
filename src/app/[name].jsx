59d4cfd1021c6ced1c5b6d7fe4476a771ac0d5d6
Eden Pearson <pearsoneden@gmail.com>
2024-03-06 14:57:21 +1300

    setup the graphql api using stepzen

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -2,9 +2,11 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
 import exercises from '../../assets/data/exercises.json'
 import { Stack } from 'expo-router'
+import { useState } from 'react'
 
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams()
+  const [isInstructionExpanded, setIsInstructionExpanded] = useState(false)
 
   const exercise = exercises.find((item) => item.name === params.name)
 
@@ -16,8 +18,17 @@ export default function ExerciseDetailsScreen() {
     <ScrollView contentContainerStyle={styles.container}>
       <Stack.Screen options={{ title: exercise.name }} />
       <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <Text style={styles.instructions}>
+      <Text
+        style={styles.instructions}
+        numberOfLines={isInstructionExpanded ? 0 : 3}
+      >
         Instructions: {exercise.instructions}
+      </Text>
+      <Text
+        onPress={() => setIsInstructionExpanded(!isInstructionExpanded)}
+        style={styles.seeMore}
+      >
+        {isInstructionExpanded ? 'See less' : 'See more'}
       </Text>
     </ScrollView>
   )
@@ -41,5 +52,9 @@ const styles = StyleSheet.create({
   instructions: {
     fontSize: 16,
     lineHeight: 20,
+  },
+  seeMore: {
+    alignSelf: 'center',
+    color: 'gray',
   },
 })
